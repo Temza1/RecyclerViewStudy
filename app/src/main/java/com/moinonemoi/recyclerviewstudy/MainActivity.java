cec77f3ba8ee95df9e9efdb5455315db16ec8234
Temza1 <artem.zateev.98@mail.ru>
2023-01-27 16:04:59 +0300

    add Gift

--- app/src/main/java/com/moinonemoi/recyclerviewstudy/MainActivity.java
+++ app/src/main/java/com/moinonemoi/recyclerviewstudy/MainActivity.java
@@ -6,9 +6,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.txtViewHello);
+        button1 = (Button) findViewById(R.id.buttonClickOnMe);
+        button2 = (Button) findViewById(R.id.button_second_page);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.text_how_are_you);
+                button1.setVisibility(View.GONE);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Gift.class);
+                startActivity(intent);
+            }
+        });
     }
 }
